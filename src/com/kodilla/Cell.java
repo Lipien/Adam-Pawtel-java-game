1def7ad83e1de3bf4f9c7ab8632aa3572b073cd6
Lipien <adam@pawtel.pl>
2018-12-13 23:08:07 +0100

    JavaFX game: TicTacToe (refactor: OO design, New Game button).

---
+++ src/com/kodilla/Cell.java
@@ -0,0 +1,47 @@
+package com.kodilla;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+
+public class Cell extends Pane {
+    private char player = ' ';
+
+    public Cell() {
+        this.setStyle("-fx-border-color: black");
+        this.setPrefSize(300, 400);
+
+    }
+
+    public char getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(char player) {
+        this.player = player;
+        if (player == 'X') {
+            Line line1 = new Line(30, 30, this.getWidth() - 30, this.getHeight() - 30);
+            line1.setStrokeWidth(10);
+            line1.setStroke(Color.GREEN);
+            Line line2 = new Line(30, this.getHeight() - 30, this.getWidth() - 30, 30);
+            line2.setStrokeWidth(10);
+            line2.setStroke(Color.GREEN);
+
+            getChildren().addAll(line1, line2);
+
+        } else if (player == 'O') {
+            Circle circle = new Circle(35);
+            circle.centerXProperty().bind(this.widthProperty().divide(2));
+            circle.centerYProperty().bind(this.heightProperty().divide(2));
+            circle.setStrokeWidth(10);
+            circle.setStroke(Color.GREEN);
+            circle.setFill(null);
+
+            getChildren().add(circle);
+        }
+    }
+
+
+}
--- src/com/kodilla/TicTacToeApp.java
+++ src/com/kodilla/TicTacToeApp.java
@@ -7,24 +7,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.Random;
 
-
 public class TicTacToeApp extends Application {
 
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 400;
+    private static final String TITLE = "Tic Tac Toe (JavaFX)";
     private char currentPlayer = 'X';
     private char comp = 'O';
     private Cell[][] cell = new Cell[3][3];
     private Label statusMsg = new Label("  X has to move first");
-
     private RadioButton radioButton = new RadioButton("play against PC");
+    private GridPane gridPane;
+    private BorderPane borderPane;
 
     public static void main(String[] args) {
         launch(args);
@@ -32,35 +33,33 @@ public class TicTacToeApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(5, 5, 5, 5));
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                cell[i][j] = new Cell();
-                gridPane.add(cell[i][j], i, j);
-            }
-        }
+        initBoard();
+
         statusMsg.setFont(Font.font("Verdana", 20));
-
-        BorderPane borderPane = new BorderPane();
+        borderPane = new BorderPane();
         borderPane.setPadding(new Insets(20, 20, 20, 20));
         borderPane.setCenter(gridPane);
         borderPane.setStyle("-fx-padding: 10");
         borderPane.setTop(statusMsg);
         borderPane.setBottom(null);
 
-        // Przycisk NewGame jest do zaprogramowania
         Button newGameButton = new Button("New GAME");
         newGameButton.setPrefSize(100, 20);
-//      newGameButton.setDefaultButton(true);
-//      newGameButton.setOnAction(event ->
-
+        newGameButton.setDefaultButton(true);
+        newGameButton.setOnAction(event -> {
+            try {
+                initBoard();
+                borderPane.setCenter(gridPane);
+                statusMsg.setText("  X has to move first");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
         Button exitButton = new Button("EXIT");
         exitButton.setPrefSize(100, 20);
         exitButton.setOnAction(e -> Platform.exit());
-
 
         radioButton.setSelected(true);
 
@@ -69,12 +68,24 @@ public class TicTacToeApp extends Application {
         buttonBar.setPadding(new Insets(15, 12, 15, 12));
         buttonBar.setSpacing(30);   // odstęp między przełącznikami
 
-
         borderPane.setRight(buttonBar);
-        Scene scene = new Scene(borderPane, 500, 400);
-        primaryStage.setTitle("Tic Tac Toe (JavaFX)");
+        Scene scene = new Scene(borderPane, WIDTH, HEIGHT);
+        primaryStage.setTitle(TITLE);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void initBoard() {
+        gridPane = new GridPane();
+        gridPane.setPadding(new Insets(5, 5, 5, 5));
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cell[i][j] = new Cell();
+                cell[i][j].setOnMouseClicked(e -> handleClick(e));
+                gridPane.add(cell[i][j], i, j);
+            }
+        }
     }
 
     private boolean isBoardFull() {
@@ -126,92 +137,55 @@ public class TicTacToeApp extends Application {
         return false;
     }
 
-    public class Cell extends Pane {
-        private char player = ' ';
+    private boolean compTurn() {
+        int see = 0;
+        while (see == 0) {
+            Random r = new Random();
+            int i = r.nextInt(3);
+            int j = r.nextInt(3);
 
-        public Cell() {
-            this.setStyle("-fx-border-color: black");
-            this.setPrefSize(300, 400);
-            this.setOnMouseClicked(e -> handleClick());
-        }
-
-        public char getPlayer() {
-            return player;
-        }
-
-        public void setPlayer(char player) {
-            this.player = player;
-            if (player == 'X') {
-                Line line1 = new Line(30, 30, this.getWidth() - 30, this.getHeight() - 30);
-                line1.setStrokeWidth(10);
-                line1.setStroke(Color.GREEN);
-                Line line2 = new Line(30, this.getHeight() - 30, this.getWidth() - 30, 30);
-                line2.setStrokeWidth(10);
-                line2.setStroke(Color.GREEN);
-
-                getChildren().addAll(line1, line2);
-
-            } else if (player == 'O') {
-                Circle circle = new Circle(35);
-                circle.centerXProperty().bind(this.widthProperty().divide(2));
-                circle.centerYProperty().bind(this.heightProperty().divide(2));
-                circle.setStrokeWidth(10);
-                circle.setStroke(Color.GREEN);
-                circle.setFill(null);
-
-                getChildren().add(circle);
+            if (cell[i][j].getPlayer() == ' ') {
+                cell[i][j].setPlayer(comp);
+                see = 1;
+            }
+            if (hasWon(comp)) {
+                statusMsg.setText(comp + " won! The game is over");
+                comp = ' ';
+            } else if (isBoardFull()) {
+                statusMsg.setText("Draw! The game is over");
+                comp = ' ';
             }
         }
+        return false;
+    }
 
-        private boolean compTurn() {
-            int see = 0;
-            while (see == 0) {
-                Random r = new Random();
-                int i = r.nextInt(3);
-                int j = r.nextInt(3);
-
-                if (cell[i][j].getPlayer() == ' ') {
-                    cell[i][j].setPlayer(comp);
-                    see = 1;
-                }
-                if (hasWon(comp)) {
-                    statusMsg.setText(comp + " won! The game is over");
-                    comp = ' ';
+    private void handleClick(MouseEvent e) {
+        Cell cell = (Cell) e.getSource();
+        if (radioButton.isSelected()) {
+            if (cell.getPlayer() == ' ' && currentPlayer != ' ') {
+                cell.setPlayer(currentPlayer);
+                if (hasWon(currentPlayer)) {
+                    statusMsg.setText(currentPlayer + " won !");
+                    currentPlayer = ' ';
                 } else if (isBoardFull()) {
-                    statusMsg.setText("Draw! The game is over");
-                    comp = ' ';
+                    statusMsg.setText("Draw !");
+                    currentPlayer = ' ';
+                } else {
+                    compTurn();
                 }
             }
-            return false;
-        }
-
-        private void handleClick() {
-            if (radioButton.isSelected()) {
-                if (player == ' ' && currentPlayer != ' ') {
-                    setPlayer(currentPlayer);
-                    if (hasWon(currentPlayer)) {
-                        statusMsg.setText(currentPlayer + " won !");
-                        currentPlayer = ' ';
-                    } else if (isBoardFull()) {
-                        statusMsg.setText("Draw !");
-                        currentPlayer = ' ';
-                    } else {
-                        compTurn();
-                    }
-                }
-            } else {
-                if (player == ' ' && currentPlayer != ' ') {
-                    setPlayer(currentPlayer);
-                    if (hasWon(currentPlayer)) {
-                        statusMsg.setText(currentPlayer + " won !");
-                        currentPlayer = ' ';
-                    } else if (isBoardFull()) {
-                        statusMsg.setText("Draw !");
-                        currentPlayer = ' ';
-                    } else {
-                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-                        statusMsg.setText(currentPlayer + " must play");
-                    }
+        } else {
+            if (cell.getPlayer() == ' ' && currentPlayer != ' ') {
+                cell.setPlayer(currentPlayer);
+                if (hasWon(currentPlayer)) {
+                    statusMsg.setText(currentPlayer + " won !");
+                    currentPlayer = ' ';
+                } else if (isBoardFull()) {
+                    statusMsg.setText("Draw !");
+                    currentPlayer = ' ';
+                } else {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                    statusMsg.setText(currentPlayer + " must play");
                 }
             }
         }
